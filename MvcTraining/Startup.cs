4f6bf59b17a081a7203290180da23ed8aeb0adc5
Гербут Евгений <gekas@i.ua>
2016-08-22 16:17:59 +0300

    Init

---
+++ MvcTraining/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcTraining.Startup))]
+namespace MvcTraining
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
